f86996c30ab34cfc760f2e64ebfc7ede33fdc45a
Daniela Lopez <33258340+DanielaLopezB@users.noreply.github.com>
2020-07-19 17:38:03 -0500

    Add files via upload

--- openSurvey.test.py
+++ openSurvey.test.py
@@ -12,10 +12,10 @@ from main import app
 class BasicTestCase(unittest.TestCase):
     def test_openSurvey(self):
             tester = app.test_client(self)
-            response = tester.get('/api/v1/assessment/test/mode/179/undefined', content_type='json')
+            response = tester.get('/api/v1/survey/test/179/true', content_type='json')
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {"Status": 1, "Message": "success", "Data": {
-            "IsInviteOnly": False, "IsEligible": True}})
+            self.assertEqual(json.loads(response.get_data().decode(sys.getdefaultencoding())), {"Status": 1, "Message": "Success", "Data": []
+            })
 
     
 
